60577ca1b46703469271b85b17777a816ccc39d3
KyleWard0525 <kward60@uncc.edu>
2021-05-09 16:31:34 -0400

    Updated

--- guitar_contr.py
+++ guitar_contr.py
@@ -52,7 +52,7 @@ def handleButtonPress():
                 else:
                     blinkOrange()
             
-            # Disable LEDs
+            # Disable LED's
             else:
                 cleanup()
 
